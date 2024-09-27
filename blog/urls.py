28ef3e0bc3bc42f5fef2207b76e56a774cf36697
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-09-27 18:26:50 +0530

    Django redirct

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,9 @@ from . import views
 
 urlpatterns=[
     path("",views.index, name="index"),
+    path("detail/<int:user_id>",views.detail, name="detail"),
+    path("new_url",views.new_url, name="new_url"),
+    path("old_url",views.old_url, name="old_url"),
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("<h1>wello come to My Blog website<h1>");
+    return HttpResponse("<h1>wello come to My Blog website<h1>")
 
+def detail(request, user_id):
+    return HttpResponse(f"hello your are visiting page of user {user_id} now")
 
+def new_url(request):
+    return HttpResponse("<h1>Redirct sucessfull<h1>")
 
+def old_url(request):
+    return redirect("new_url")
