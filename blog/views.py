ef9efc1c79b0713d6536fadb241f580566199e68
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-10-04 12:41:13 +0530

    dynamic page created

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,17 @@ from django.urls import reverse
 
 
 def index(request):
-    return render(request,'templates/index.html')
+    blog_title = "Latest Posts"
+
+    posts =[
+        {'id':1, 'title':'Post 1', 'content':'Content of post 1'},
+        {'id':2, 'title':'Post 2', 'content':'Content of post 2'},
+        {'id':3, 'title':'Post 3', 'content':'Content of post 3'},
+        {'id':4, 'title':'Post 4', 'content':'Content of post 4'},
+        {'id':5, 'title':'Post 5', 'content':'Content of post 5'},
+    ]
+
+    return render(request,'templates/index.html', {'blog_title': blog_title, 'posts':posts})
 
 def detail(request,user_id):
     return render(request,'templates/details.html')
