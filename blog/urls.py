9cc5c947d7855bb34bb610bb86f524d6e88d0383
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-10-06 16:13:19 +0530

    post page dynamic content added

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name= "blog"
 
 urlpatterns=[
     path("",views.index, name="index"),
-    path("post/<int:post_id>", views.detail, name="detail"),
+    path("post/<str:post_id>", views.detail, name="detail"),
     path("new_test_url",views.new_url, name="new_url"),
     path("old_url",views.old_url, name="old_url"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,29 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.urls import reverse
+import logging
+
+
+
+posts =[
+    {'id':1, 'title':'Post 1', 'content':'Content of post 1'},
+    {'id':2, 'title':'Post 2', 'content':'Content of post 2'},
+    {'id':3, 'title':'Post 3', 'content':'Content of post 3'},
+    {'id':4, 'title':'Post 4', 'content':'Content of post 4'},
+    {'id':5, 'title':'Post 5', 'content':'Content of post 5'},
+]
 
 
 def index(request):
     blog_title = "Latest Posts"
-
-    posts =[
-        {'id':1, 'title':'Post 1', 'content':'Content of post 1'},
-        {'id':2, 'title':'Post 2', 'content':'Content of post 2'},
-        {'id':3, 'title':'Post 3', 'content':'Content of post 3'},
-        {'id':4, 'title':'Post 4', 'content':'Content of post 4'},
-        {'id':5, 'title':'Post 5', 'content':'Content of post 5'},
-    ]
-
     return render(request,'templates/index.html', {'blog_title': blog_title, 'posts':posts})
 
-def detail(request,user_id):
-    return render(request,'templates/details.html')
+def detail(request,post_id):
+    post=next((item for item in posts if item['id']== int(post_id)), None)
+    # logger=logging.getLogger("Testing")
+    # logger.debug(f"post variable is {post}")
+    return render(request,'templates/details.html',{"post":post})
+
 
 def new_url(request):
     return HttpResponse("<h1>Redirct sucessfull<h1>")
