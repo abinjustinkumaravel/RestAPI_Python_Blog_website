bce50111f4b331417182eb0e699261398d8d87e4
abinjustinkumaravel <abinjustinkumaravel@gmail.com>
2024-10-11 12:21:04 +0530

    Category models were added

--- blog/management/commands/__init__.py
+++ blog/management/commands/__init__.py
@@ -1,1 +1,2 @@
 from .populate_post import Command as PopulatePostsCommand
+from .populate_categories import Command as PopulateCategoriesCommand
--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.utils.text import slugify
 
+class Category(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
 
@@ -9,27 +15,12 @@ class Post(models.Model):
     img_url= models.URLField(max_length=300,null=True)
     created_at =models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(unique=True, null=True, blank=True)
-
+    category =models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
         super().save(*args, **kwargs)
     
-    # def save(self, *args, **kwargs):
-    # # Check if the slug is empty or needs to be updated
-    #     if not self.slug:
-    #         base_slug = slugify(self.title)
-    #         slug = base_slug
-    #         counter = 1
-    #         # Ensure slug is unique by appending a number if necessary
-    #         while Post.objects.filter(slug=slug).exists():
-    #             slug = f"{base_slug}-{counter}"
-    #             counter += 1
-    #         self.slug = slug
-    #     super().save(*args, **kwargs)
-
-
     def __str__(self):
         return self.title
-
     
